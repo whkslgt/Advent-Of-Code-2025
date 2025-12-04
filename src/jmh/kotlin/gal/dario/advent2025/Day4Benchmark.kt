23e012eea6ec8bee508570dfc7361f8f2d181afa
Dario Lemos <dario.lemos@masabi.com>
2025-12-04 11:02:58 +0100

    feat: solution for Day 4 Part 1

---
+++ src/jmh/kotlin/gal/dario/advent2025/Day4Benchmark.kt
@@ -0,0 +1,25 @@
+package gal.dario.advent2025
+
+import org.openjdk.jmh.annotations.*
+import java.util.concurrent.TimeUnit
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 3, time = 1)
+@Measurement(iterations = 5, time = 1)
+@Fork(2)
+open class Day4Benchmark {
+
+    private lateinit var day4: Day4
+
+    @Setup
+    fun setup() {
+        day4 = Day4()
+    }
+
+    @Benchmark
+    fun benchmarkCalculateAccessibleRolls(): Int {
+        return day4.calculateAccessibleRolls()
+    }
+}
---
+++ src/main/kotlin/gal/dario/advent2025/Day4.kt
@@ -0,0 +1,69 @@
+package gal.dario.advent2025
+
+
+class Day4 {
+    companion object {
+        const val INPUT_FILE = "/day-4-input.txt"
+    }
+
+    private fun readInput(): Sequence<String> {
+        return object {}.javaClass
+            .getResourceAsStream(INPUT_FILE)!!
+            .bufferedReader()
+            .lineSequence()
+    }
+
+    private data class Coordinate(val row: Int, val col: Int)
+    private data class Cell(val hasRoll: Boolean, var visited: Boolean = false)
+
+    fun calculateAccessibleRolls(): Int {
+        val matrix = mutableMapOf<Coordinate, Cell>()
+        var maxRow = 0
+        var maxCol = 0
+
+        readInput().forEachIndexed { row, line ->
+            maxRow = maxOf(maxRow, row)
+            line.forEachIndexed { col, char ->
+                maxCol = maxOf(maxCol, col)
+                matrix[Coordinate(row, col)] = Cell(char == '@')
+            }
+        }
+
+        var accessibleCount = 0
+        for (row in 0..maxRow) {
+            for (col in 0..maxCol) {
+                val coord = Coordinate(row, col)
+                val currentCell = matrix[coord]
+                if (!currentCell!!.visited && isAccessible(matrix, currentCell, coord)) {
+                    accessibleCount++
+                }
+            }
+        }
+
+        return accessibleCount
+    }
+
+    private fun isAccessible(matrix: Map<Coordinate, Cell>, currentCell: Cell, coord: Coordinate): Boolean {
+        if (!currentCell.hasRoll) return false
+        currentCell.visited = true
+
+        val neighbors = listOfNotNull(
+            coord.copy(row = coord.row - 1),
+            coord.copy(row = coord.row - 1, col = coord.col + 1),
+            coord.copy(row = coord.row - 1, col = coord.col - 1),
+            coord.copy(row = coord.row + 1),
+            coord.copy(row = coord.row + 1, col = coord.col + 1),
+            coord.copy(row = coord.row + 1, col = coord.col - 1),
+            coord.copy(col = coord.col - 1),
+            coord.copy(col = coord.col + 1)
+        )
+
+        return neighbors.filter { neighbor ->
+            matrix[neighbor]?.hasRoll ?: false
+        }.size < 4
+    }
+}
+
+fun main() {
+    println("Accessible rolls: ${Day4().calculateAccessibleRolls()}")
+}
