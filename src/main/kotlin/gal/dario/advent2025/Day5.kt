41e46d8208eecdd3c14fe60286786887c9620b52
Dario Lemos <dario.lemos@masabi.com>
2025-12-05 10:20:53 +0100

    feat: solution for Day 5 Part 1

---
+++ src/main/kotlin/gal/dario/advent2025/Day5.kt
@@ -0,0 +1,63 @@
+package gal.dario.advent2025
+
+import java.util.BitSet
+
+class Day5 {
+    companion object {
+        const val INPUT_FILE = "/day-5-input.txt"
+    }
+
+    private fun readInput(): Sequence<String> {
+        return object {}.javaClass
+            .getResourceAsStream(INPUT_FILE)!!
+            .bufferedReader()
+            .lineSequence()
+    }
+
+    fun calculateFreshIngredients(): Int {
+        val input = readInput().iterator()
+        val mergedRanges = ranges(input)
+
+        var totalCount = 0
+        while(input.hasNext()) {
+            val ingredient = input.next().toLong()
+            if (mergedRanges.any { ingredient in it }) {
+                totalCount++
+            }
+        }
+
+        return totalCount
+    }
+
+    private fun ranges(input: Iterator<String>): MutableList<LongRange> {
+        val ranges = mutableListOf<LongRange>()
+
+        while (input.hasNext()) {
+            val line = input.next()
+            if (line.isBlank()) break
+            val (from, to) = line.split('-').map { it.toLong() }
+            ranges.add(LongRange(from, to))
+        }
+
+        ranges.sortBy { it.first }
+
+        val mergedRanges = mutableListOf<LongRange>()
+        var currentRange = ranges[0]
+
+        for (i in 1 until ranges.size) {
+            val nextRange = ranges[i]
+            if (nextRange.first <= currentRange.last) {
+                currentRange = LongRange(currentRange.first, maxOf(currentRange.last, nextRange.last))
+            } else {
+                mergedRanges.add(currentRange)
+                currentRange = nextRange
+            }
+        }
+        mergedRanges.add(currentRange)
+        return mergedRanges
+    }
+}
+
+fun main() {
+    println("Fresh ingredients: ${Day5().calculateFreshIngredients()}")
+}
